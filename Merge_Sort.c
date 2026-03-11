4fd40b322802ba620f49f2c05927949f16dad05c
dhyansirigerics24 <dhyansirigeri.cs24@bmsce.ac.in>
2026-03-11 10:38:54 +0530

    Add files via upload

--- Merge_Sort.c
+++ Merge_Sort.c
@@ -1,4 +1,5 @@
 #include<stdio.h>
+#include<time.h>
 
 void merge(int arr[],int left,int mid,int right){
     int i,j,k;
@@ -38,13 +39,32 @@ void mergeSort(int arr[],int left,int right){
 
 int main(){
     int n,i;
-    printf("Enter number of elements:");
+
+    clock_t start,end;
+    double CPU_time;
+
+    printf("Enter number of elements: ");
     scanf("%d",&n);
+
     int arr[n];
-    printf("Enter the array elements:");
-    for(i=0;i<n;i++) scanf("%d",&arr[i]);
+
+    printf("Enter the array elements: ");
+    for(i=0;i<n;i++) 
+        scanf("%d",&arr[i]);
+
+    start = clock();  
+
     mergeSort(arr,0,n-1);
-    printf("Array after sorting:");
-    for(i=0;i<n;i++) printf("%d\t",arr[i]);
+
+    end = clock();     
+
+    CPU_time = ((double)(end-start))/CLOCKS_PER_SEC;
+
+    printf("\nArray after sorting:\n");
+    for(i=0;i<n;i++) 
+        printf("%d\t",arr[i]);
+
+    printf("\nExecution Time: %f seconds\n",CPU_time);
+
     return 0;
 }
