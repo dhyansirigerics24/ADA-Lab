6e686ba0fced094ac2b9158f3086e5b4007488d4
dhyansirigerics24 <dhyansirigeri.cs24@bmsce.ac.in>
2026-03-11 10:24:34 +0530

    Add files via upload

---
+++ Merge_Sort.c
@@ -0,0 +1,50 @@
+#include<stdio.h>
+
+void merge(int arr[],int left,int mid,int right){
+    int i,j,k;
+    int nl=mid-left+1;
+    int nr=right-mid;
+    int L[nl],R[nr];
+    for(i=0;i<nl;i++) L[i]=arr[left+i];
+    for(j=0;j<nr;j++) R[j]=arr[mid+1+j];
+    i=0;
+    j=0;
+    k=left;
+    while(i<nl && j<nr){
+        if(L[i]<R[j]){
+            arr[k++]=L[i++];
+        }
+        else{
+            arr[k++]=R[j++];
+        }
+    }
+    while(i<nl){
+        arr[k++]=L[i++];
+    }
+    while(j<nr){
+        arr[k++]=R[j++];
+    }
+}
+
+void mergeSort(int arr[],int left,int right){
+    if(left<right){
+        int mid;
+        mid=(left+right)/2;
+        mergeSort(arr,left,mid);
+        mergeSort(arr,mid+1,right);
+        merge(arr,left,mid,right);
+    }
+}
+
+int main(){
+    int n,i;
+    printf("Enter number of elements:");
+    scanf("%d",&n);
+    int arr[n];
+    printf("Enter the array elements:");
+    for(i=0;i<n;i++) scanf("%d",&arr[i]);
+    mergeSort(arr,0,n-1);
+    printf("Array after sorting:");
+    for(i=0;i<n;i++) printf("%d\t",arr[i]);
+    return 0;
+}
